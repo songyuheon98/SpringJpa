d7ec372ce82e7a4cb75e68992b4c8fd89f72e400
songyuheon98 <songyuheon2750@gmail.com>
2023-09-13 22:48:16 +0900

    first

---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,83 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class OrderItem {
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    //    @Column(name="ITEM_ID")
+//    private Long itemId;
+    @ManyToOne
+    @JoinColumn(name="ORDER_ID")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name="ITEM_ID")
+    private Item item;
+
+//    @Column(name="ORDER_ID")
+//    private Long orderId;
+
+    private int orderPrice;
+
+    private int count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+//    public Long getItemId() {
+//        return itemId;
+//    }
+//
+//    public void setItemId(Long itemId) {
+//        this.itemId = itemId;
+//    }
+//
+//    public Long getOrderId() {
+//        return orderId;
+//    }
+//
+//    public void setOrderId(Long orderId) {
+//        this.orderId = orderId;
+//    }
+
+    public int getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(int orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
